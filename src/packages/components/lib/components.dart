1207f778cc2f14457dbaf3d149d2b5a245b61828
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-10-25 00:18:26 +0700

    feat(ui): add text theme for poppins

---
+++ src/packages/components/lib/components.dart
@@ -0,0 +1,4 @@
+/// Packages for components
+library components;
+
+export 'src/theme/text_theme/text_theme.dart';
---
+++ src/packages/components/lib/src/theme/text_theme/poppins_theme.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:components/src/theme/text_theme/base_text_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// {@template Poppins}
+/// Packages for components
+/// {@endtemplate}
+extension Poppins on TextTheme {
+  TextStyle get h0RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h0Regular);
+  TextStyle get h0SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h0SemiBold);
+  TextStyle get h0BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h0Bold);
+  TextStyle get h0ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h0ExtraBold);
+  TextStyle get h1RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h1Regular);
+  TextStyle get h1SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h1SemiBold);
+  TextStyle get h1BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h1Bold);
+  TextStyle get h1ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h1ExtraBold);
+  TextStyle get h2RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h2Regular);
+  TextStyle get h2SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h2SemiBold);
+  TextStyle get h2BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h2Bold);
+  TextStyle get h2ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h2ExtraBold);
+  TextStyle get h3RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h3Regular);
+  TextStyle get h3SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h3SemiBold);
+  TextStyle get h3BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h3Bold);
+  TextStyle get h3ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h3ExtraBold);
+  TextStyle get h4RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h4Regular);
+  TextStyle get h4SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h4SemiBold);
+  TextStyle get h4BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h4Bold);
+  TextStyle get h4ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h4ExtraBold);
+  TextStyle get h5RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h5Regular);
+  TextStyle get h5SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h5SemiBold);
+  TextStyle get h5BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h5Bold);
+  TextStyle get h5ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h5ExtraBold);
+  TextStyle get h6RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h6Regular);
+  TextStyle get h6SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h6SemiBold);
+  TextStyle get h6BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h6Bold);
+  TextStyle get h6ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().h6ExtraBold);
+  TextStyle get bodyRegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().bodyRegular);
+  TextStyle get bodySemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().bodySemiBold);
+  TextStyle get bodyBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().bodyBold);
+  TextStyle get bodyExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().bodyExtraBold);
+  TextStyle get sub1RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().sub1Regular);
+  TextStyle get sub1SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().sub1SemiBold);
+  TextStyle get sub1BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().sub1Bold);
+  TextStyle get sub1ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().sub1ExtraBold);
+  TextStyle get sub2RegularPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().sub2Regular);
+  TextStyle get sub2SemiBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().sub2SemiBold);
+  TextStyle get sub2BoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().sub2Bold);
+  TextStyle get sub2ExtraBoldPoppins =>
+      GoogleFonts.poppins(textStyle: const BaseTextTheme().sub2ExtraBold);
+}
