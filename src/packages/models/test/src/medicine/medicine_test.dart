4f6216c5d9bc9e590862b8f2dbdb4a3e3bc4eb76
nmvinhdl1215 <nmvinhdl1215@gmail.com>
2023-10-24 18:45:46 +0700

    feat(models): add code for create medicine

---
+++ src/packages/models/test/src/medicine/medicine_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:models/models.dart';
+
+void main() {
+  // Unit tests for the `Medicine` class
+  group('Medicine', () {
+    // Test the `copyWith` method
+    test('copyWith', () {
+      // Create a `Medicine` instance
+      final penicillin = Medicine(
+        name: 'penicillin',
+        unit: 'mg',
+      );
+
+      // Copy penicillin without changing any properties
+      final amlodipine = penicillin.copyWith();
+
+      // Verify the copy
+      expect(amlodipine.name, 'penicillin');
+      expect(amlodipine.unit, 'mg');
+
+      //Copy peniciilin and change its properties
+      final amoxicillin = penicillin.copyWith(name: 'amoxicillin', unit: 'ml');
+
+      expect(amoxicillin.name, 'amoxicillin');
+      expect(amoxicillin.unit, 'ml');
+
+      //JSON object
+      final json = {
+        'name': 'penicillin',
+        'unit': 'mg',
+      };
+
+      //To instance
+      final penicillinFromJson = Medicine.fromJson(json);
+      expect(penicillinFromJson, penicillin);
+
+      //To JSON
+      final jsonFromPenicillin = penicillin.toJson();
+      expect(jsonFromPenicillin, json);
+    });
+  });
+}
