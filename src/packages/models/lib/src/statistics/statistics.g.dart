66ed7309387c5c07e2517e392c4c86ad27d888bd
quocle <tlquoc03@gmail.com>
2023-10-25 17:01:49 +0700

    feat(models): add model for statistics

---
+++ src/packages/models/lib/src/statistics/statistics.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'statistics.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Statistics _$StatisticsFromJson(Map<String, dynamic> json) {
+  $checkKeys(
+    json,
+    requiredKeys: const ['id', 'value', 'categoryName', 'prescriptionID'],
+  );
+  return Statistics(
+    id: json['id'] as int,
+    value: json['value'] as int,
+    categoryName: json['categoryName'] as String,
+    prescriptionID: json['prescriptionID'] as String,
+    note: json['note'] as String?,
+  );
+}
+
+Map<String, dynamic> _$StatisticsToJson(Statistics instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'value': instance.value,
+      'categoryName': instance.categoryName,
+      'prescriptionID': instance.prescriptionID,
+      'note': instance.note,
+    };
