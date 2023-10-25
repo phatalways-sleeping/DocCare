90acf4e4592f7b26de22676097ca943c55963508
quocle <tlquoc03@gmail.com>
2023-10-25 16:46:01 +0700

    feat(models): add model for absent_request

---
+++ src/packages/models/lib/src/absent_request/absent_request.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'absent_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AbsentRequest _$AbsentRequestFromJson(Map<String, dynamic> json) {
+  $checkKeys(
+    json,
+    requiredKeys: const [
+      'doctorID',
+      'date',
+      'doctorName',
+      'dateRequest',
+      'isApproved'
+    ],
+  );
+  return AbsentRequest(
+    doctorID: json['doctorID'] as String,
+    date: DateTime.parse(json['date'] as String),
+    doctorName: json['doctorName'] as String,
+    dateRequest: json['dateRequest'] as String,
+    reason: json['reason'] as String?,
+    isApproved: json['isApproved'] as bool,
+  );
+}
+
+Map<String, dynamic> _$AbsentRequestToJson(AbsentRequest instance) =>
+    <String, dynamic>{
+      'doctorID': instance.doctorID,
+      'date': instance.date.toIso8601String(),
+      'doctorName': instance.doctorName,
+      'dateRequest': instance.dateRequest,
+      'reason': instance.reason,
+      'isApproved': instance.isApproved,
+    };
