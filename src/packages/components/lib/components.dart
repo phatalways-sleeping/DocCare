ee51291a9db4d6b94ba65a8c4b8ed9beeb2ea1c3
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-10-26 16:25:35 +0700

    feat(ui): add svg assets

--- src/packages/components/lib/components.dart
+++ src/packages/components/lib/components.dart
@@ -1,5 +1,6 @@
 /// Packages for components
 library components;
 
+export 'src/svgs/dc_svgs.dart';
 export 'src/theme/color_scheme/color_scheme.dart';
 export 'src/theme/text_theme/text_theme.dart';
