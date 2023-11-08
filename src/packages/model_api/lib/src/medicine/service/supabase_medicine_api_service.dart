fccd370707e3d4c4ee5bf44408d443d3e40844ca
nmvinhdl1215 <nmvinhdl1215@gmail.com>
2023-11-08 23:45:01 +0700

    Revert "Revert "Merge pull request #10 from phatalways-sleeping/feature/apis/medicine_intake""
    This reverts commit 23ad1ee1574cd5f01d2efbba30d50175497f0fb0.

---
+++ src/packages/model_api/lib/src/medicine/service/supabase_medicine_api_service.dart
@@ -0,0 +1,79 @@
+import 'package:models/models.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:model_api/src/medicine/service/medicine_api_service.dart';
+
+class SupabaseMedicineApiService implements MedicineAPIService<Medicine> {
+  const SupabaseMedicineApiService({
+    required this.supabase,
+  });
+
+  final SupabaseClient supabase;
+
+  @override
+  Future<Medicine> getMedicine(String name) => supabase
+      .from('medicine')
+      .select<PostgrestList>()
+      .eq('name', name)
+      .then(
+        (value) => value.isEmpty
+            ? throw Exception('No medicine found')
+            : Medicine.fromJson(value.first),
+      )
+      .onError((error, stackTrace) => throw Exception(error));
+
+  @override
+  Future<List<Medicine>> getAllMedicineList() => supabase
+      .from('medicine')
+      .select<PostgrestList>()
+      .then(
+        (value) => value.isEmpty
+            ? throw Exception('No medicine found')
+            : value.map(Medicine.fromJson).toList(),
+      )
+      .onError((error, stackTrace) => throw Exception(error));
+
+  @override
+  Future<List<Medicine>> getMedicineList(List<String> names) => supabase
+      .from('medicine')
+      .select<PostgrestList>()
+      .in_('name', names)
+      .then(
+        (value) => value.isEmpty
+            ? throw Exception('No medicine found')
+            : value.map(Medicine.fromJson).toList(),
+      )
+      .onError((error, stackTrace) => throw Exception(error));
+
+  @override
+  Future<void> createMedicine(Medicine medicine) => supabase
+      .from('medicine')
+      .insert(
+        medicine.toJson(),
+      )
+      .onError((error, stackTrace) => throw Exception(error));
+
+  //For either medicine in solid or liquid form
+  @override
+  Future<void> updateMedicineUnit(String name, String unit) => supabase
+      .from('medicine')
+      .update({
+        'unit': unit,
+      })
+      .eq('name', name)
+      .onError((error, stackTrace) => throw Exception(error));
+
+  @override
+  Future<void> deleteMedicine(String name) => supabase
+      .from('medicine')
+      .delete()
+      .eq('name', name)
+      .onError((error, stackTrace) => throw Exception(error));
+
+  //Stream of a single medicine
+  @override
+  Stream<Medicine> streamMedicine(String name) => supabase
+      .from('medicine')
+      .stream(primaryKey: ['name'])
+      .eq('name', name)
+      .map((event) => Medicine.fromJson(event.first));
+}
