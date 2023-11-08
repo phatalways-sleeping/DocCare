7025f2dfb58e9a2250cae1093f5ad44847eac3ee
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2023-11-09 01:27:26 +0700

    style(api): delete redundant import

--- src/packages/model_api/test/src/prescription/prescription_api_test.dart
+++ src/packages/model_api/test/src/prescription/prescription_api_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:model_api/src/prescription/service/supabase_prescription_api.dart';
-import 'package:models/models.dart';
 
 void main() {
   group('PrescriptionApi', () {
