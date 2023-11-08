3c74a0ace1a11e5cb51bb78bbbb3bbc009e64288
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2023-11-09 01:38:25 +0700

    fix(model): export symbols to model_api

--- src/packages/model_api/lib/model_api.dart
+++ src/packages/model_api/lib/model_api.dart
@@ -1,4 +1,7 @@
 /// Package for A.P.I related operations on models
 library model_api;
 
-export 'src/model_api.dart';
+//export 'src/model_api.dart';
+export 'src/category/service/supabase_category_api_service.dart';
+export 'src/prescription/service/supabase_prescription_api.dart';
+export 'src/working_shift/service/supabase_working_shift_api.dart';
