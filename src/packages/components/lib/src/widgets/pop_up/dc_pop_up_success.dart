4741719ece8f386b566275bfc5ae8bfde90d9af1
nmvinhdl1215 <nmvinhdl1215@gmail.com>
2023-11-08 21:40:13 +0700

    feat(ui): add pop-up for success and failed

---
+++ src/packages/components/lib/src/widgets/pop_up/dc_pop_up_success.dart
@@ -0,0 +1,65 @@
+import 'package:components/components.dart';
+import 'package:extensions/extensions.dart';
+import 'package:flutter/material.dart';
+
+class DCPopupSuccess extends StatelessWidget {
+  const DCPopupSuccess({
+    Key? key,
+    required this.title,
+    required this.message,
+    required this.buttonText,
+  }) : super(key: key);
+
+  final String title;
+  final String message;
+  final String buttonText;
+
+  @override
+  Widget build(BuildContext context) {
+    //Center everything in the middle column using Alert Dialog
+    return AlertDialog(
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+      ),
+      titleTextStyle: TextStyle(
+        fontFamily: context.textTheme.h6BoldPoppins.fontFamily,
+        fontSize: 20,
+        color: context.colorScheme.primary,
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image.asset(
+            'images/thumbs-up.png',
+            height: 100,
+            width: 100,
+          ),
+          Text(
+            message,
+            style: TextStyle(
+              fontFamily: context.textTheme.h6BoldPoppins.fontFamily,
+              color: context.colorScheme.primary,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              fontFamily: context.textTheme.h6BoldPoppins.fontFamily,
+              color: context.colorScheme.primary,
+            ),
+          ),
+        ),
+      ],
+      actionsAlignment: MainAxisAlignment.center,
+    );
+  }
+}
