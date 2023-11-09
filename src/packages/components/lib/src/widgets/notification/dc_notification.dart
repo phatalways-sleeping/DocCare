e718408ef6c72ff8a30cc2b7104a7cf96c12e513
PhucChuHoang <phucchu390@gmail.com>
2023-11-09 13:03:54 +0700

    refactor(ui): add customization over widget

--- src/packages/components/lib/src/widgets/notification/dc_notification.dart
+++ src/packages/components/lib/src/widgets/notification/dc_notification.dart
@@ -8,73 +8,107 @@ class DCNotification extends StatelessWidget {
   const DCNotification({
     required this.title,
     required this.message,
-    required this.notificationTime,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.onPressed,
+    this.notificationTime,
+    this.haveNotificationTime = false,
+    this.heightFactor = 0.15,
+    this.widthFactor = 0.8,
+    this.textStyleTitle,
+    this.textStyleMessage,
     super.key,
-  });
+  })  : assert(
+          !haveNotificationTime || notificationTime != null,
+          'If haveNotificationTime is true, notificationTime must not be null',
+        ),
+        assert(
+            heightFactor >= 0.15 &&
+                heightFactor <= 1.0 &&
+                widthFactor >= 0.8 &&
+                widthFactor <= 1.0,
+            'heightFactor must be between 0.15 and 1.0 and widthFactor must be between 0.8 and 1.0');
 
-  final String title;
-  final String message;
-  final DateTime notificationTime;
+  final Widget title;
+  final Widget message;
+  final DateTime? notificationTime;
+  final bool haveNotificationTime;
+  final Color backgroundColor;
+  final Color textColor;
+  final double heightFactor;
+  final double widthFactor;
+  final TextStyle? textStyleTitle;
+  final TextStyle? textStyleMessage;
+  final void Function(BuildContext context) onPressed;
 
   @override
   Widget build(BuildContext context) {
-    final timeDisplay = DateFormat('hh:mm a, d MMM y').format(notificationTime);
+    var time = '';
+    if (haveNotificationTime) {
+      time = DateFormat('hh:mm a, d MMM y').format(notificationTime!);
+    }
 
-    return ElevatedButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(
-          context.colorScheme.quinary,
-        ),
-        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-          const EdgeInsets.all(16),
-        ),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+    return FractionallySizedBox(
+      heightFactor: heightFactor,
+      widthFactor: widthFactor,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.hovered)) {
+              return backgroundColor.withOpacity(0.8);
+            } else if (states.contains(MaterialState.pressed)) {
+              return backgroundColor.withOpacity(0.6);
+            } else {
+              return backgroundColor;
+            }
+          }),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+            const EdgeInsets.all(16),
           ),
-        ),
-      ),
-      onPressed: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Notification clicked'),
-          ),
-        );
-      },
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment:
-            CrossAxisAlignment.start, // Align text to the left side
-        children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(
-              fontFamily: context.textTheme.h6BoldPoppins.fontFamily,
-              fontSize: 20,
-              color: context.colorScheme.onPrimary,
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
           ),
-          const SizedBox(height: 8),
-          Text(
-            message,
-            style: TextStyle(
-              fontFamily: context.textTheme.h6RegularPoppins.fontFamily,
-              color: context.colorScheme.onPrimary,
+        ),
+        onPressed: () => onPressed(context),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment:
+              CrossAxisAlignment.start, // Align text to the left side
+          children: <Widget>[
+            DefaultTextStyle.merge(
+              style: textStyleTitle ??
+                  context.textTheme.h6BoldPoppins.copyWith(
+                    color: textColor,
+                    fontSize: 20,
+                  ),
+              child: title,
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                timeDisplay,
-                style: TextStyle(
-                  fontFamily: context.textTheme.h6RegularPoppins.fontFamily,
-                  color: context.colorScheme.onPrimary,
+            const SizedBox(height: 8),
+            DefaultTextStyle.merge(
+              style: textStyleMessage ??
+                  context.textTheme.h6RegularPoppins.copyWith(
+                    color: textColor,
+                    fontSize: 14,
+                  ),
+              child: message,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                DefaultTextStyle.merge(
+                  style: textStyleMessage ??
+                      context.textTheme.h6RegularPoppins.copyWith(
+                        color: textColor,
+                        fontSize: 14,
+                      ),
+                  child: Text(time),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
