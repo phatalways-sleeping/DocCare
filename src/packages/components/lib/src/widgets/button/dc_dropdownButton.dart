8a317ffe30daec6d2991aa65ee3d75d374e39b1c
quocle <tlquoc03@gmail.com>
2023-11-10 00:13:29 +0700

    feat(ui): Added reusable dropdown button

---
+++ src/packages/components/lib/src/widgets/button/dc_dropdownButton.dart
@@ -0,0 +1,35 @@
+import 'package:components/components.dart';
+import 'package:extensions/extensions.dart';
+import 'package:flutter/material.dart';
+
+
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
+
+
+class DropdownMenuExample extends StatefulWidget {
+  const DropdownMenuExample({super.key});
+
+  @override
+  State<DropdownMenuExample> createState() => _DropdownMenuExampleState();
+}
+
+class _DropdownMenuExampleState extends State<DropdownMenuExample> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<String>(
+      initialSelection: list.first,
+      onSelected: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
+        return DropdownMenuEntry<String>(value: value, label: value);
+      }).toList(),
+    );
+  }
+}
