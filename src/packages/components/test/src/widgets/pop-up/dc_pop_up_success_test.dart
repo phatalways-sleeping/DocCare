ed274be4aab854cdd6c8d8ca0c0fde1bd437d296
nmvinhdl1215 <nmvinhdl1215@gmail.com>
2023-11-09 18:42:56 +0700

    test(ui): Update test for 2 type of pop_up

--- src/packages/components/test/src/widgets/pop-up/dc_pop_up_success_test.dart
+++ src/packages/components/test/src/widgets/pop-up/dc_pop_up_success_test.dart
@@ -1,6 +1,8 @@
+import 'package:components/src/widgets/pop_up/base_pop_up.dart';
 import 'package:components/src/widgets/pop_up/dc_pop_up_success.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   group('DCPopUpSuccess', () {
@@ -8,32 +10,64 @@ void main() {
       await tester.pumpWidget(
         MaterialApp(
           home: DCPopupSuccess(
-            title: 'title',
-            message: 'message',
-            buttonText: 'buttonText',
+            title: 'Title',
+            message: 'Message',
+            buttonText: 'Button Text',
           ),
         ),
       );
       expect(find.byType(DCPopupSuccess), findsOneWidget);
 
-      expect(find.text('title'), findsOneWidget);
+      //Base popup has to be here
+      expect(find.byType(BasePopup), findsOneWidget);
 
-      expect(find.text('message'), findsOneWidget);
+      expect(find.text('Title'), findsOneWidget);
+
+      expect(find.text('Message'), findsOneWidget);
+
+      final button = tester.widget<ElevatedButton>(
+        find.byType(ElevatedButton),
+      );
+
+      expect(button, isNotNull);
+    });
+
+    testWidgets('Render with optional size', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: DCPopupSuccess(
+            title: 'Title',
+            message: 'Message',
+            buttonText: 'Button Text',
+            iconWidth: 50,
+            iconHeight: 50,
+          ),
+        ),
+      );
+      expect(find.byType(DCPopupSuccess), findsOneWidget);
+
+      //Icon has to have the optional size
+      final icon = tester.widget<SvgPicture>(
+        find.byType(SvgPicture),
+      );
+
+      expect(icon.width, 50);
+      expect(icon.height, 50);
     });
 
     testWidgets('Close pop-up', (WidgetTester tester) async {
       await tester.pumpWidget(
         MaterialApp(
           home: DCPopupSuccess(
-            title: 'title',
-            message: 'message',
-            buttonText: 'buttonText',
+            title: 'Title',
+            message: 'Message',
+            buttonText: 'Button Text',
           ),
         ),
       );
       expect(find.byType(DCPopupSuccess), findsOneWidget);
 
-      await tester.tap(find.text('buttonText'));
+      await tester.tap(find.text('Button Text'));
       await tester.pumpAndSettle();
       expect(find.byType(DCPopupSuccess), findsNothing);
     });
