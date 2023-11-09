bd3d1f86b362a79c3a5dc16c83f7c1a4f40c53cf
quocle <tlquoc03@gmail.com>
2023-11-10 02:02:14 +0700

    fix(ui): complete dropdown button

--- src/packages/components/lib/src/widgets/button/dc_dropdownButton.dart
+++ src/packages/components/lib/src/widgets/button/dc_dropdownButton.dart
@@ -2,39 +2,40 @@ import 'package:components/components.dart';
 import 'package:extensions/extensions.dart';
 import 'package:flutter/material.dart';
 
-
-const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
-
-
-
 class DropdownMenuExample extends StatefulWidget {
-  const DropdownMenuExample({
-    this.width,
-    Key? key,
+  final List<String> items;
+  final double dropdownWidth; // New parameter for dropdown width
 
+const DropdownMenuExample({
+    required this.items,
+    this.dropdownWidth = 200.0, // Default dropdown width if not provided
+    Key? key,
   }) : super(key: key);
 
-  final double? width;
-
   @override
-  State<DropdownMenuExample> createState() => _DropdownMenuExampleState();
+  State<DropdownMenuExample> createState() => _DropdownMenuExampleState(items: items, dropdownWidth: dropdownWidth);
 }
 
 class _DropdownMenuExampleState extends State<DropdownMenuExample> {
-  String dropdownValue = list.first;
+  late String dropdownValue;
+  late double dropdownWidth;
+
+  _DropdownMenuExampleState({required List<String> items, required this.dropdownWidth}) {
+    dropdownValue = items.first;
+  }
 
   @override
   Widget build(BuildContext context) {
     return DropdownMenu<String>(
-      width: 300,
-      initialSelection: list.first,
+      width: dropdownWidth, 
+      initialSelection: dropdownValue,
       onSelected: (String? value) {
         // This is called when the user selects an item.
         setState(() {
           dropdownValue = value!;
         });
       },
-      dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
+      dropdownMenuEntries: widget.items.map<DropdownMenuEntry<String>>((String value) {
         return DropdownMenuEntry<String>(value: value, label: value);
       }).toList(),
     );
