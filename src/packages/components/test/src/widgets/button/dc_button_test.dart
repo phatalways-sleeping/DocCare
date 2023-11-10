4d308fd065881b0e504aa63eef5aab32a18ef8b4
quocle <tlquoc03@gmail.com>
2023-11-10 11:24:28 +0700

    test(ui): test for Button

---
+++ src/packages/components/test/src/widgets/button/dc_button_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:components/src/widgets/button/dc_button.dart';
+
+void main() {
+  group('DCButton Widget Tests', () {
+    testWidgets('DCButton renders correctly', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: DCButton(
+              key: Key('myButton'),
+              onTap: () {}, // Provide a simple empty callback for testing
+              text: 'Press Me',
+            ),
+          ),
+        ),
+      );
+
+      // Verify that DCButton widget is rendered.
+      expect(find.byKey(Key('myButton')), findsOneWidget);
+
+      // Verify that the text is rendered.
+      expect(find.text('Press Me'), findsOneWidget);
+       
+    });
+
+  });
+}
