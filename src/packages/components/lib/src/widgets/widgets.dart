b3307e9afb2eab1865d621a6330493730dedcd0b
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-11-11 12:17:23 +0700

    feat(ui): add concrete version of drawer for each role

--- src/packages/components/lib/src/widgets/widgets.dart
+++ src/packages/components/lib/src/widgets/widgets.dart
@@ -1,2 +1,5 @@
+export 'drawer/dc_admin_drawer.dart';
+export 'drawer/dc_customer_drawer.dart';
+export 'drawer/dc_doctor_drawer.dart';
 export 'notification/dc_notification.dart';
 export 'text_form_field/text_form_field.dart';
