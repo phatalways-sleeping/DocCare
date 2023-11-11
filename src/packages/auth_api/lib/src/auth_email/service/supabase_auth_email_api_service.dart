75652bcad259964e571a830cd3c1733644249c5c
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2023-11-11 15:55:12 +0700

    refactor(auth): Split error handler out of Class, Use different Class Object to handle

--- src/packages/auth_api/lib/src/auth_email/service/supabase_auth_email_api_service.dart
+++ src/packages/auth_api/lib/src/auth_email/service/supabase_auth_email_api_service.dart
@@ -1,22 +1,22 @@
 import 'auth_email_api_service.dart';
-import 'package:flutter/services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:auth_api/src/auth_email/error_handler/auth_email_api_error_handler.dart';
 
 class SupabaseAuthEmailApiService implements AuthEmailApiService {
   const SupabaseAuthEmailApiService({
     required this.supabase,
+    required this.errorHandler,
   });
 
   final SupabaseClient supabase;
+  final AuthEmailApiErrorHandler errorHandler;
 
   @override
   Future<void> signUpWithEmailPassword(String email, String password) async {
     try {
       await supabase.auth.signUp(email: email, password: password);
-    } on PlatformException catch (e) {
-      handleAuthException(e);
     } catch (e) {
-      throw AuthException('Sign-up failed. Please try again.');
+      errorHandler.handleAuthException(e);
     }
   }
 
@@ -24,10 +24,8 @@ class SupabaseAuthEmailApiService implements AuthEmailApiService {
   Future<void> signInWithEmailPassword(String email, String password) async {
     try {
       await supabase.auth.signInWithPassword(email: email, password: password);
-    } on PlatformException catch (e) {
-      handleAuthException(e);
     } catch (e) {
-      throw AuthException('Sign-in failed. Please try again.');
+      errorHandler.handleAuthException(e);
     }
   }
 
@@ -36,7 +34,7 @@ class SupabaseAuthEmailApiService implements AuthEmailApiService {
     try {
       await supabase.auth.signOut();
     } catch (e) {
-      throw AuthException('Sign-out failed. Please try again.');
+      errorHandler.handleAuthException(e);
     }
   }
 
@@ -44,35 +42,8 @@ class SupabaseAuthEmailApiService implements AuthEmailApiService {
   Future<void> sendPasswordResetEmail(String email) async {
     try {
       await supabase.auth.resetPasswordForEmail(email);
-    } on PlatformException catch (e) {
-      handleAuthException(e);
     } catch (e) {
-      throw AuthException('Password reset failed. Please try again.');
+      errorHandler.handleAuthException(e);
     }
   }
-
-  void handleAuthException(PlatformException e) {
-    // Handle specific platform exceptions and throw custom AuthException
-    if (e.code == 'MISSING_EMAIL') {
-      throw AuthException('Email is required.');
-    } else if (e.code == 'MISSING_PASSWORD') {
-      throw AuthException('Password is required.');
-    } else if (e.code == 'INVALID_CREDENTIALS') {
-      throw AuthException('Invalid email or password.');
-    } else if (e.code == 'WEAK_PASSWORD') {
-      throw AuthException('Password is too weak.');
-    } else if (e.code == 'EMAIL_ALREADY_IN_USE') {
-      throw AuthException('Email is already in use.');
-    } else if (e.code == 'USER_NOT_FOUND') {
-      throw AuthException('Email not found.');
-    } else {
-      throw AuthException('Authentication failed. Please try again.');
-    }
-  }
-}
-
-class AuthException implements Exception {
-  final String message;
-
-  AuthException(this.message);
 }
