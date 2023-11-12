1adc57c538bca97b7955d9e178de09d29ed4b96b
nmvinhdl1215 <nmvinhdl1215@gmail.com>
2023-11-12 15:20:40 +0700

    feat(ui): Added more options for base pop-up

--- src/packages/components/lib/src/widgets/pop_up/base_pop_up.dart
+++ src/packages/components/lib/src/widgets/pop_up/base_pop_up.dart
@@ -4,77 +4,147 @@ import 'package:flutter/material.dart';
 
 class BasePopup extends StatelessWidget {
   const BasePopup({
-    required this.title,
     required this.message,
-    required this.buttonText,
-    required this.buttonColor,
-    required this.popupIcon,
-    this.textColor,
+    required this.buttonsText,
+    this.buttonsWidth,
+    this.buttonsHeight,
+    this.buttonsColor,
+    this.buttonsTextSize,
+    this.buttonsTextColors,
+    this.popupIcon,
     this.popupIconWidth,
     this.popupIconHeight,
+    this.iconBackgroundColor,
+    this.title,
+    this.titleTextSize,
+    this.titleAlignment,
+    this.titleTextColor,
+    this.messageTextColor,
+    this.messageTextSize,
+    this.messageAlignment,
+    this.onPopupButtonClicked,
     super.key,
   });
 
-  final String title;
-  final String message;
-  final String buttonText;
-  final Color buttonColor;
-  final Widget popupIcon;
-  final Color? textColor;
+  final Widget? title;
+  final Color? iconBackgroundColor;
+  //Message is in a list so that many message can be stack on each other
+  final List<Widget> message;
+  final List<Color>? messageTextColor;
+  final List<double>? messageTextSize;
+  final List<TextAlign>? messageAlignment;
+  final List<Text> buttonsText;
+  final List<Color>? buttonsColor;
+  final double? buttonsWidth;
+  final double? buttonsHeight;
+  final double? buttonsTextSize;
+  final Color? buttonsTextColors;
+  final Widget? popupIcon;
   final double? popupIconWidth;
   final double? popupIconHeight;
+  final Color? titleTextColor;
+  final double? titleTextSize;
+  final TextAlign? titleAlignment;
+  final List<Function>? onPopupButtonClicked;
 
   @override
   Widget build(BuildContext context) {
-    //Center everything in the middle column using Alert Dialog
     return AlertDialog(
-      title: Text(
-        title,
-        textAlign: TextAlign.center,
-      ),
-      titleTextStyle: TextStyle(
-        fontFamily: context.textTheme.h6BoldPoppins.fontFamily,
-        fontSize: 20,
-        color: textColor ?? context.colorScheme.primary,
+      //set up title with title alignment, title text size and title text color
+      title: DefaultTextStyle.merge(
+        style: context.textTheme.h4ExtraBoldPoppins.copyWith(
+          color: titleTextColor ?? context.colorScheme.primary,
+          fontSize: titleTextSize ?? 24,
+        ),
+        textAlign: titleAlignment ?? TextAlign.center,
+        child: title ?? Container(),
       ),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Container(
-            width: popupIconWidth ?? 100,
-            height: popupIconHeight ?? 100,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: buttonColor,
-            ),
-            child: Transform.scale(
-              scale: 0.75,
-              child: popupIcon,
+          //Setup icon for popup
+          //If has icon size, setup size, else don't do anything
+          Flexible(
+            //scale the background circle to be a bit bigger than the icon
+            child: FractionallySizedBox(
+              child: Container(
+                width: popupIconHeight,
+                height: popupIconWidth,
+                decoration: BoxDecoration(
+                  color: iconBackgroundColor ?? context.colorScheme.primary,
+                  shape: BoxShape.circle,
+                ),
+                child: Transform.scale(
+                  scale: 0.75,
+                  child: Center(
+                    child: popupIcon ?? Container(),
+                  ),
+                ),
+              ),
             ),
           ),
-          Text(
-            message,
-            style: TextStyle(
-              fontFamily: context.textTheme.h6BoldPoppins.fontFamily,
-              color: textColor ?? context.colorScheme.primary,
+          //Set up message with message alignment, message text size and message text color
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              message.length,
+              (index) => DefaultTextStyle.merge(
+                style: context.textTheme.h4BoldPoppins.copyWith(
+                  color:
+                      messageTextColor?[index] ?? context.colorScheme.primary,
+                  fontSize: messageTextSize?[index] ?? 16,
+                ),
+                textAlign: messageAlignment?[index] ?? TextAlign.center,
+                child: Column(
+                  children: [
+                    message[index],
+                    const SizedBox(height: 8),
+                  ],
+                ),
+              ),
             ),
-            textAlign: TextAlign.center,
           ),
         ],
       ),
-      actions: [
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: buttonColor,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text(
-            buttonText,
-            style: TextStyle(
-              fontFamily: context.textTheme.h6BoldPoppins.fontFamily,
-              color: textColor ?? context.colorScheme.primary,
+      actions: <Widget>[
+        //set up button with button alignment
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            buttonsText.length,
+            (index) => Padding(
+              padding: const EdgeInsets.only(right: 8),
+              //Create buttons with different pressed method
+              child: SizedBox(
+                width: buttonsHeight,
+                height: buttonsWidth,
+                child: ElevatedButton(
+                  onPressed: () {
+                    //If there is a pressed method, call it
+                    if (onPopupButtonClicked != null &&
+                        onPopupButtonClicked?[index] != null) {
+                      // ignore: avoid_dynamic_calls
+                      onPopupButtonClicked?[index](context);
+                    }
+                    //Else close the popup
+                    else {
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        buttonsColor?[index] ?? context.colorScheme.primary,
+                  ),
+                  child: DefaultTextStyle.merge(
+                    style: context.textTheme.h4ExtraBoldPoppins.copyWith(
+                      color: buttonsTextColors ?? context.colorScheme.primary,
+                      fontSize: buttonsTextSize ?? 16,
+                    ),
+                    textAlign: titleAlignment ?? TextAlign.center,
+                    child: title ?? Container(),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
