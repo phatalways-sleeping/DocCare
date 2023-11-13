badae46866f13a7baf282eceb6d044fdd8f523da
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-11-13 18:40:21 +0700

    build(ui): add exports

---
+++ src/packages/components/lib/src/widgets/pop_up/dc_pop_up.dart
@@ -0,0 +1,5 @@
+export 'dc_pop_up_cancellation.dart';
+export 'dc_pop_up_confirm.dart';
+export 'dc_pop_up_doctor_cancel.dart';
+export 'dc_pop_up_failed.dart';
+export 'dc_pop_up_success.dart';
--- src/packages/components/lib/src/widgets/widgets.dart
+++ src/packages/components/lib/src/widgets/widgets.dart
@@ -1,1 +1,4 @@
+export 'pop_up/dc_pop_up.dart';
 export 'text_form_field/text_form_field.dart';
+
+
