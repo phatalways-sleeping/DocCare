5749c4f75a3fb305144a197b384e6795e0b4efbf
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-11-16 15:18:13 +0700

    refactor(api): auth_api refactor

---
+++ src/packages/authentication/auth_api/lib/auth_api.dart
@@ -0,0 +1,6 @@
+/// Package for A.P.I related operations on authentication operations and service
+library auth_api;
+
+export 'src/auth_email/error/auth_email_api_error.dart';
+export 'src/auth_email/service/auth_email_api_service.dart';
+export 'src/auth_email/service/supabase_auth_email_api_service.dart';
---
+++ src/packages/authentication/auth_api/lib/src/auth_api.dart
@@ -0,0 +1,7 @@
+/// {@template auth_api}
+/// Package for A.P.I related operations on authentication operations and service
+/// {@endtemplate}
+class AuthApi {
+  /// {@macro auth_api}
+  const AuthApi();
+}
