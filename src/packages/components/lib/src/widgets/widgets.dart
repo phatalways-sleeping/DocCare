640d324528253f7e14eee558816d450377c206bc
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-11-17 09:24:48 +0700

    build(common): add export of button

--- src/packages/components/lib/src/widgets/widgets.dart
+++ src/packages/components/lib/src/widgets/widgets.dart
@@ -1,11 +1,14 @@
 export 'animation/animated_box.dart';
 export 'button/dc_button.dart';
 export 'button/dc_dropdown_button.dart';
+export 'button/dc_filled_button.dart';
+export 'button/dc_outlined_button.dart';
 export 'drawer/dc_admin_drawer.dart';
 export 'drawer/dc_customer_drawer.dart';
 export 'drawer/dc_doctor_drawer.dart';
 export 'navigation_bar/navigation_bar.dart';
 export 'notification/dc_notification.dart';
 export 'text_form_field/text_form_field.dart';
+
 
 
