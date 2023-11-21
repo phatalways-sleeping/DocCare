394bf8c96b019ee95455b13bc203e5b6726348db
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2023-11-21 17:38:03 +0700

    fix(ui): change test file

--- src/packages/components/test/src/widgets/header_bar/dc_header_bar_test.dart
+++ src/packages/components/test/src/widgets/header_bar/dc_header_bar_test.dart
@@ -1,74 +1,91 @@
 import 'package:components/components.dart';
-import 'package:components/src/widgets/header_bar/dc_customer_header_bar.dart';
+import 'package:components/src/widgets/header_bar/base_header_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('DCHeaderBar', () {
-    testWidgets('Render DCHeaderBar', (WidgetTester tester) async {
+  group('BaseHeaderBar', () {
+    testWidgets('Render BaseHeaderBar', (WidgetTester tester) async {
       await tester.pumpWidget(
         MaterialApp(
           home: BaseHeaderBar(
-            title: 'Test Title',
-            iconType: HeaderIconType.back,
-            onPressed: () {}, // Mock callback
-            onNotificationPressed: () {}, // Mock callback
+            headerBarTitle: 'Test Title',
+            leadingItems: DCHeaderBarItem(
+              prefixIcon: Icon(Icons.arrow_back),
+              onTap: (BuildContext context) {},
+            ),
+            actionItems: [
+              DCHeaderBarItem(
+                prefixIcon: Icon(Icons.notification_important),
+                onTap: (BuildContext context) {},
+              ),
+            ],
           ),
         ),
       );
-      expect(find.byType(DCCustomerHeaderBar), findsOneWidget);
 
+      expect(find.byType(BaseHeaderBar), findsOneWidget);
       expect(find.text('Test Title'), findsOneWidget);
-
-      expect(find.byType(IconButton),
-          findsNWidgets(2)); // Two icons (drawer and notification)
+      expect(find.byType(AppBar), findsOneWidget);
+      expect(find.byType(DCHeaderBarItem), findsNWidgets(2));
     });
 
-    testWidgets('Trigger Drawer Icon Callback', (WidgetTester tester) async {
-      bool drawerCallbackTriggered = false;
+    testWidgets('Trigger Leading Icon Callback', (WidgetTester tester) async {
+      bool leadingCallbackTriggered = false;
 
       await tester.pumpWidget(
         MaterialApp(
           home: BaseHeaderBar(
-            title: 'Test Title',
-            iconType: HeaderIconType.back,
-            onPressed: () {
-              drawerCallbackTriggered = true;
-            },
-            onNotificationPressed: () {}, // Mock callback
+            headerBarTitle: 'Test Title',
+            leadingItems: DCHeaderBarItem(
+              prefixIcon: Icon(Icons.arrow_back),
+              onTap: (BuildContext context) {
+                leadingCallbackTriggered = true;
+              },
+            ),
+            actionItems: [
+              DCHeaderBarItem(
+                prefixIcon: Icon(Icons.notification_important),
+                onTap: (BuildContext context) {},
+              ),
+            ],
           ),
         ),
       );
 
-      await tester
-          .tap(find.byType(IconButton).first); // Tap the first icon (drawer)
+      await tester.tap(find.byType(IconButton).first);
       await tester.pumpAndSettle();
 
-      expect(drawerCallbackTriggered, true);
+      expect(leadingCallbackTriggered, true);
     });
 
-    testWidgets('Trigger Notification Icon Callback',
-        (WidgetTester tester) async {
-      bool notificationCallbackTriggered = false;
+    testWidgets('Trigger Action Icon Callback', (WidgetTester tester) async {
+      bool actionCallbackTriggered = false;
 
       await tester.pumpWidget(
         MaterialApp(
           home: BaseHeaderBar(
-            title: 'Test Title',
-            iconType: HeaderIconType.back,
-            onPressed: () {}, // Mock callback
-            onNotificationPressed: () {
-              notificationCallbackTriggered = true;
-            },
+            headerBarTitle: 'Test Title',
+            leadingItems: DCHeaderBarItem(
+              prefixIcon: Icon(Icons.arrow_back),
+              onTap: (BuildContext context) {},
+            ),
+            actionItems: [
+              DCHeaderBarItem(
+                prefixIcon: Icon(Icons.notification_important),
+                onTap: (BuildContext context) {
+                  actionCallbackTriggered = true;
+                },
+              ),
+            ],
           ),
         ),
       );
 
-      await tester.tap(
-          find.byType(IconButton).last); // Tap the last icon (notification)
+      await tester.tap(find.byType(IconButton).last);
       await tester.pumpAndSettle();
 
-      expect(notificationCallbackTriggered, true);
+      expect(actionCallbackTriggered, true);
     });
   });
 }
