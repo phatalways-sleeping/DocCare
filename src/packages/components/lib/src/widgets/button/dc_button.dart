1e8dadf81d6deb9ca4232aa928662aadddec771e
quocle <tlquoc03@gmail.com>
2023-11-21 22:40:50 +0700

    fix(ui): height and width base on Context

--- src/packages/components/lib/src/widgets/button/dc_button.dart
+++ src/packages/components/lib/src/widgets/button/dc_button.dart
@@ -17,12 +17,13 @@ class DCButton extends StatelessWidget {
     this.borderWidth,
     this.pressedOpacity,
     this.hoveredOpacity,
-    this.buttonHeight,
-    this.buttonWidth,
+    this.heightFactor,
+    this.widthFactor,
   }) : assert(
-          textSize == null || (textSize! > 0 && textSize! < 80),
-          'If textSize is provided, it should be between 0 and 80',
+          textSize == null || (textSize! > 0 && textSize! < 100),
+          'If textSize is provided, it should be between 0 and 100',
         ),
+        
         super(key: key);
   final void Function(BuildContext context) onPressed;
   final Color? borderColor;
@@ -36,12 +37,21 @@ class DCButton extends StatelessWidget {
   final double? borderWidth;
   final double? pressedOpacity;
   final double? hoveredOpacity;
-  final double? buttonWidth;
-  final double? buttonHeight;
-
+  final double? heightFactor; 
+  final double? widthFactor;
 
   @override
   Widget build(BuildContext context) {
+     final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    final double calculatedButtonWidth = widthFactor != null
+        ? screenWidth * widthFactor! // Calculate button width based on widthFactor
+        : screenWidth * 0.3; // Default to 50% of screen width
+
+    final double calculatedButtonHeight = heightFactor != null
+        ? screenHeight * heightFactor! // Calculate button height based on heightFactor
+        : screenHeight * 0.06; // Default to 10% of screen height
     final DocCareLightColorScheme colorScheme = DocCareLightColorScheme();
     final textWidget = Text(
       text,
@@ -58,7 +68,7 @@ class DCButton extends StatelessWidget {
       child: OutlinedButton(
         style: ButtonStyle(
           padding: const MaterialStatePropertyAll(EdgeInsets.zero),
-          minimumSize: MaterialStateProperty.all(Size(buttonWidth ?? 100, buttonHeight ?? 40)),
+          minimumSize: MaterialStateProperty.all(Size(calculatedButtonWidth, calculatedButtonHeight)),
           side: MaterialStateProperty.resolveWith((states) {
             double width = borderWidth ?? 0.0;
             final borderColor =
