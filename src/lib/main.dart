68ac2f01c998aaf3d7478749545f3835cf78e42f
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-11-22 19:30:19 +0700

    fix(ui): remove appbar in  main.dart

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -120,11 +120,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: DCCustomerHeaderBar(
-        headerBarTitle: "DocCare",
-        haveBack: true,
-        haveNotification: true,
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
