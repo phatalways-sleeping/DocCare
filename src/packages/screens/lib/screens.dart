3fa9d5b24978eb53714dfcee57d30603b7f39ad1
PhucChuHoang <phucchu390@gmail.com>
2023-12-15 17:40:53 +0700

    feat(ui): add screen for displaying notification

--- src/packages/screens/lib/screens.dart
+++ src/packages/screens/lib/screens.dart
@@ -4,3 +4,4 @@ library screens;
 export 'src/change_password/dc_change_password_screen.dart';
 export 'src/sign_up/dc_sign_up_screen.dart';
 export 'src/splash/dc_splash_screen.dart';
+export 'src/notification/dc_notification_screen.dart';
