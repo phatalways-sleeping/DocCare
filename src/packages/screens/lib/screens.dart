026e00b4c2126e57c909d422fe95a48aa4c178af
phat-always-sleeping <tkphat21@apcs.fitus.edu.vn>
2023-12-18 10:17:28 +0700

    build(common): export scheduling screen

--- src/packages/screens/lib/screens.dart
+++ src/packages/screens/lib/screens.dart
@@ -2,6 +2,7 @@
 library screens;
 
 export 'src/booking/flows/dc_doctor_view_main_screen.dart';
+export 'src/booking/flows/dc_schedule_view_screen.dart';
 export 'src/change_password/dc_change_password_screen.dart';
 export 'src/profile/dc_profile.dart';
 export 'src/report/dc_admin_generate_report.dart';
