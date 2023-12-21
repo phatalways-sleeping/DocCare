2022a5f5e591aa18d57acb1f7409e903ba5893b2
PhucChuHoang <phucchu390@gmail.com>
2023-12-21 23:15:42 +0700

    feat(ui): export to screens library

--- src/packages/views/lib/screens.dart
+++ src/packages/views/lib/screens.dart
@@ -1,4 +1,6 @@
 /// Package for screens in this application
 library screens;
+
 export 'src/screens/authentication/sign_up/dc_sign_up_screen.dart';
 export 'src/screens/authentication/splash/dc_splash_screen.dart';
+export 'src/screens/users/home/dc_customer_home_screen.dart';
