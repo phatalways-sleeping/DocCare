2e2a6177ea3c6978089183e6fcded757bbba2174
PhucChuHoang <phucchu390@gmail.com>
2024-01-03 16:11:51 +0700

    fix(ui): fix upcoming appointments

--- src/packages/views/lib/src/screens/doctors/home/controller/doctor_home_bloc.dart
+++ src/packages/views/lib/src/screens/doctors/home/controller/doctor_home_bloc.dart
@@ -1,8 +1,9 @@
+// ignore_for_file: public_member_api_docs, avoid_dynamic_calls
+
 import 'package:bloc/bloc.dart';
+import 'package:controllers/controllers.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
-import 'package:controllers/controllers.dart';
 import 'package:models/models.dart';
 import 'package:utility/utility.dart';
 
@@ -17,72 +18,83 @@ class DoctorHomeBloc extends Bloc<DoctorHomeEvent, DoctorHomeState> {
   ) : super(DoctorHomeInitial.empty()) {
     on<DoctorHomeLoadEvent>(_onDoctorHomeLoadEvent);
     on<DoctorHomeOpenDoctorScheduleViewEvent>(
-        _onDoctorHomeOpenDoctorScheduleViewEvent);
+      _onDoctorHomeOpenDoctorScheduleViewEvent,
+    );
     on<DoctorHomeResetEvent>(_onDoctorHomeResetEvent);
     on<DoctorHomeOpenCancelAppointmentViewEvent>(
-        _onDoctorHomeOpenCancelAppointmentViewEvent);
+      _onDoctorHomeOpenCancelAppointmentViewEvent,
+    );
   }
   final DoctorRepositoryService? _doctorRepositoryService;
   //final AuthenticationRepositoryService authenticationRepositoryService;
   final GlobalKey<NavigatorState> _navigatorKey;
   final NotificationManagerService _notificationManagerService;
 
-  void _onDoctorHomeLoadEvent(
+  Future<void> _onDoctorHomeLoadEvent(
     DoctorHomeLoadEvent event,
     Emitter<DoctorHomeState> emit,
   ) async {
     try {
+      // ignore: lines_longer_than_80_chars
       // remove this 2 lines when login is done and doctor id is passed from login screen
-      final id = 'D001';
+      const id = 'D001';
       _doctorRepositoryService!.initializeDoctorId(id);
 
-      final List<dynamic> appointments =
+      final appointments =
           await _doctorRepositoryService!.getAppointmentsByDoctorId();
       final dynamic profileData =
           await _doctorRepositoryService!.getProfileData();
-      final String doctorName = profileData['fullName'].toString();
+      final doctorName = profileData['fullName'].toString();
 
       // get doctor name after changing the doctor table in the database
       // final String doctorAvatarUrl = profileData['imgPath'];
       // just hard code for now
-      final String doctorAvatarUrl =
+      const doctorAvatarUrl =
           'https://pics.craiyon.com/2023-07-05/a8e9e1290f08447bb300681ce2b563e9.webp';
 
-      int upcomingAppointmentIndex = -1;
-      String upcomingCustomerName = '';
-      Map<String, List<dynamic>> appointmentMap = {};
+      var upcomingAppointmentIndex = -1;
+      var upcomingCustomerName = '';
+      final appointmentMap = <String, List<dynamic>>{};
+      // ignore: lines_longer_than_80_chars
       // get the index of the upcoming appointment, which is after current time and closest to current time
       // also map the appointments to each date
 
-      for (int i = 0; i < appointments.length; i++) {
+      for (var i = 0; i < appointments.length; i++) {
         if (appointments[i]['isCanceled'] == true) continue;
-        DateTime tmp = DateTime.parse(appointments[i]['date'].toString());
+        final tmp = DateTime.parse(appointments[i]['date'].toString());
         //tmp = DateTime(tmp.year, tmp.month, tmp.day, 0, 0, 0);
-        int hour =
-            (int.parse(appointments[i]['period'].toString()) / 2).toInt() + 7;
-        int minute = (int.parse(appointments[i]['period'].toString()) % 2) * 30;
-        DateTime exactTime =
+        final hour = int.parse(appointments[i]['period'].toString()) ~/ 2 + 7;
+        final minute =
+            (int.parse(appointments[i]['period'].toString()) % 2) * 30;
+        final exactTime =
             DateTime(tmp.year, tmp.month, tmp.day, hour, minute, 0);
 
         if (exactTime.isAfter(DateTime.now()) ||
             exactTime.isAtSameMomentAs(DateTime.now())) {
-          if (upcomingAppointmentIndex == -1 ||
-              tmp.isBefore(DateTime.parse(
-                  appointments[upcomingAppointmentIndex]['date'].toString())) ||
-              (tmp.isAtSameMomentAs(DateTime.parse(
-                      appointments[upcomingAppointmentIndex]['date']
-                          .toString())) &&
-                  int.parse(appointments[i]['period'].toString()) <
-                      int.parse(appointments[upcomingAppointmentIndex]['period']
-                          .toString()))) {
-            upcomingAppointmentIndex = i;
+          if (appointments[i]['done'] != true &&
+              appointments[i]['done'] != 'true') {
+            if (upcomingAppointmentIndex == -1 ||
+                tmp.isBefore(
+                  DateTime.parse(
+                    appointments[upcomingAppointmentIndex]['date'].toString(),
+                  ),
+                ) ||
+                (tmp.isAtSameMomentAs(
+                      DateTime.parse(
+                        appointments[upcomingAppointmentIndex]['date']
+                            .toString(),
+                      ),
+                    ) &&
+                    int.parse(appointments[i]['period'].toString()) <
+                        int.parse(
+                          appointments[upcomingAppointmentIndex]['period']
+                              .toString(),
+                        ))) {
+              upcomingAppointmentIndex = i;
+            }
           }
         }
-        String convertedDate = tmp.year.toString() +
-            '-' +
-            tmp.month.toString() +
-            '-' +
-            tmp.day.toString();
+        final convertedDate = '${tmp.year}-${tmp.month}-${tmp.day}';
         if (appointmentMap.containsKey(convertedDate)) {
           appointmentMap[convertedDate]!.add(appointments[i]);
         } else {
@@ -92,8 +104,9 @@ class DoctorHomeBloc extends Bloc<DoctorHomeEvent, DoctorHomeState> {
       if (upcomingAppointmentIndex != -1) {
         upcomingCustomerName =
             appointments[upcomingAppointmentIndex]['customerName'].toString();
-      } else
+      } else {
         upcomingCustomerName = 'no appointment';
+      }
 
       emit(
         DoctorHomeLoadedSuccess(
@@ -150,12 +163,12 @@ class DoctorHomeBloc extends Bloc<DoctorHomeEvent, DoctorHomeState> {
     );
   }
 
-  void _onDoctorHomeOpenCancelAppointmentViewEvent(
+  Future<void> _onDoctorHomeOpenCancelAppointmentViewEvent(
     DoctorHomeOpenCancelAppointmentViewEvent event,
     Emitter<DoctorHomeState> emit,
   ) async {
     try {
-      Appointment appointment = Appointment(
+      final appointment = Appointment(
         customerID: event.appointment['customerID'].toString(),
         doctorID: event.appointment['doctorID'].toString(),
         period: int.parse(event.appointment['period'].toString()),
